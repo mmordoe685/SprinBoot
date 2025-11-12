df5258f520322fa8977310ba1075832afad3ab55
mmordoe685 <mmordoe685@g.educaand.es>
2025-11-12 09:29:10 +0100

    Inicio Springboot

---
+++ libreria/src/test/java/com/biblioteca/libreria/LibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.libreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
